07ca63c641eee4d52646b3e6ea7c796e2f198e0e
guillaumequillet <bgachats@yahoo.fr>
2019-09-22 17:08:52 +0200

    small typo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('controller/EpisodeListController.php');
 require_once('controller/EpisodeController.php');
 require_once('controller/CommentController.php');
 
-// some checks mus be added
+// some checks must be added
 $action  = isset($_GET['action']) ? $_GET['action'] : 'episode';
 $episode = isset($_GET['id']) ? $_GET['id'] : 0;
 
