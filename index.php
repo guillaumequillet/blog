6b04daf40bffc4bcc276609f2301982b7f5281a1
guillaumequillet <bgachats@yahoo.fr>
2019-09-25 10:47:36 +0200

    refactoring : prepare for EpisodeController

--- index.php
+++ index.php
@@ -11,4 +11,13 @@ declare(strict_types=1);
 
 // this file is the ROOTER
 // it will only create ONE single Controller and call a SINGLE ACTION on it
+/*
+controller=episode&action=show&param=2
+controller=episode&action=add
+controller=episode&action=delete&param=3
+controller=episode&action=edit&param=4
 
+controller=comment&action=add&param=2
+controller=comment&action=delete&param=3
+controller=comment&action=report&param=4
+*/
