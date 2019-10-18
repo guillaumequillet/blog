21d6acc6d2084072e2ffb4016d2afc2171cb3e55
guillaumequillet <bgachats@yahoo.fr>
2019-10-18 09:03:15 +0200

    set excerpt size

--- src/controller/EpisodeController.php
+++ src/controller/EpisodeController.php
@@ -38,6 +38,7 @@ class EpisodeController extends Controller
 
     public function showList(?int $page = 0) : void {
         $episodesPerPage = 3;
+        $excerptSize     = 200;
 
         if (is_null($page)) {
             $page = 1;
@@ -58,7 +59,7 @@ class EpisodeController extends Controller
             $this->data['episodeExcerptsList'][] = [
                 'id'             => $episode['id'],
                 'title'          => $episode['title'],
-                'contentExcerpt' => substr(strip_tags(html_entity_decode($episode['content'])), 0, 5)
+                'contentExcerpt' => substr(strip_tags(html_entity_decode($episode['content'])), 0, $excerptSize)
             ];    
         }
 
