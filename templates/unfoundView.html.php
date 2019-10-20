687ad4e46d6c6700ca02bc03248b9fcc115c14f3
guillaumequillet <bgachats@yahoo.fr>
2019-10-20 15:01:04 +0200

    polished 404 error page

--- templates/unfoundView.html.php
+++ templates/unfoundView.html.php
@@ -1,5 +1,9 @@
 <?php declare(strict_types=1); ?>
 
 <h2>Erreur d'adresse</h2>
-<p>OOOPS !</p>
-<p>L'épisode n'a pas été trouvé.</p>
+<p>Nous sommes désolés, mais la page indiquée n'a pas été trouvée. Nous vous invitons à utiliser le menu de navigation pour retourner aux épisodes.</p>
+
+<figure>
+	<img src="images/error_404.jpg" alt="Paysage vierge Alaska">
+	<figcaption>Une étendue sauvage à l'adresse indiquée</figcaption>
+</figure>
