02a45ab31f47c60df22469d6e0d88e315cd437e7
guillaumequillet <bgachats@yahoo.fr>
2019-10-20 12:47:45 +0200

    improved episodeAdmin table

--- templates/adminEpisodesView.html.php
+++ templates/adminEpisodesView.html.php
@@ -1,29 +1,45 @@
 <?php declare(strict_types=1); ?>
 
-<h2>Gestion des épisodes</h2>
+<h2>Administration des Episodes</h2>
 
 <a href="index.php?controller=admin&action=editEpisode" class="button">Ajouter un épisode</a>
 
 <table>
-    <tr>
-        <td>ID</td>
-        <td>Titre</td>
-        <td>Publié</td>
-        <td>Date de publication</td>
-        <td>Action</td>
-    </tr>
+    <caption>Liste des épisodes</caption>
+    
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Titre</th>
+            <th>Publié</th>
+            <th>Date de publication</th>
+            <th>Action</th>
+        </tr>
+    </thead>
 
-    <?php foreach($data['episodeData'] as $episode): ?>
-    <tr>
-        <td><?= $episode['id'] ?></td>
-        <td><?= $episode['title'] ?></td>
-        <td><?= $episode['published'] ? "Oui" : "Non" ?></td>
-        <td><?= $episode['publication_date'] ?></td>
-        <td>
-            <a href="index.php?controller=admin&action=editEpisode&param=<?= $episode['id'] ?>" class="button">Editer</a>
-            <a href="index.php?controller=admin&action=previewEpisode&param=<?= $episode['id'] ?>" class="button">Aperçu</a>
-            <a href="index.php?controller=admin&action=deleteEpisode&param=<?= $episode['id'] ?>" onclick="return confirm('Etes-vous sûr(e) de vouloir supprimer cet épisode ?');" class="button">Supprimer</a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
+    <tfoot>
+        <tr>
+            <td>ID</td>
+            <td>Titre</td>
+            <td>Publié</td>
+            <td>Date de publication</td>
+            <td>Action</td>
+        </tr>
+    </tfoot>
+
+    <tbody>
+        <?php foreach($data['episodeData'] as $episode): ?>
+        <tr>
+            <td><?= $episode['id'] ?></td>
+            <td><?= $episode['title'] ?></td>
+            <td><?= $episode['published'] ? "Oui" : "Non" ?></td>
+            <td><?= $episode['publication_date'] ?></td>
+            <td>
+                <a href="index.php?controller=admin&action=editEpisode&param=<?= $episode['id'] ?>" class="button">Editer</a>
+                <a href="index.php?controller=admin&action=previewEpisode&param=<?= $episode['id'] ?>" class="button">Aperçu</a>
+                <a href="index.php?controller=admin&action=deleteEpisode&param=<?= $episode['id'] ?>" onclick="return confirm('Etes-vous sûr(e) de vouloir supprimer cet épisode ?');" class="button">Supprimer</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table>
